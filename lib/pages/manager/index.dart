7bb38a712821d9e3a0ff843d7b9823938b188d73
wang.jiaqi <279298470@qq.com>
2023-10-05 17:27:50 +0800

    [feat]: 侧边栏开关

--- lib/pages/manager/index.dart
+++ lib/pages/manager/index.dart
@@ -1,12 +1,11 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:neo_cat_flutter/components/common/editor.dart';
 import 'package:neo_cat_flutter/components/manager/class_manager_view.dart';
-import 'package:neo_cat_flutter/components/manager/function_btn_group.dart';
 import 'package:neo_cat_flutter/theme/common_theme.dart';
+import 'package:neo_cat_flutter/types/manager_view_mode.dart';
 
 /// @author wang.jiaqi
 /// @date 2023-09-29 11
-
 class ManagerPage extends StatefulWidget {
   const ManagerPage({super.key});
 
@@ -15,6 +14,152 @@ class ManagerPage extends StatefulWidget {
 }
 
 class _ManagerPageState extends State<ManagerPage> {
+  ViewMode currentViewMode = ViewMode.classMode;
+  bool isBrowserPaneVisible = true;
+  bool isEditorPaneVisible = true;
+  int centerFlexWeight = 2;
+
+  void _switchViewMode() {
+    switch (currentViewMode) {
+      case ViewMode.classMode:
+        {
+          setState(() {
+            currentViewMode = ViewMode.relationMode;
+          });
+          break;
+        }
+      case ViewMode.relationMode:
+        {
+          setState(() {
+            currentViewMode = ViewMode.classMode;
+          });
+          break;
+        }
+    }
+  }
+
+  void _swichBrowserPane() {
+    setState(() {
+      if (isBrowserPaneVisible) {
+        centerFlexWeight += 1;
+      } else {
+        centerFlexWeight -= 1;
+      }
+      isBrowserPaneVisible = !isBrowserPaneVisible;
+    });
+  }
+
+  void _switchEditorPane() {
+    setState(() {
+      if (isEditorPaneVisible) {
+        centerFlexWeight += 1;
+      } else {
+        centerFlexWeight -= 1;
+      }
+      isEditorPaneVisible = !isEditorPaneVisible;
+    });
+  }
+
+  Widget _browserModeBtnBuilder() {
+    return Row(
+      children: [
+        Tooltip(
+          message: '类视图',
+          displayHorizontally: true,
+          useMousePosition: false,
+          style: const TooltipThemeData(preferBelow: true),
+          child: IconButton(
+            icon: currentViewMode == ViewMode.classMode
+                ? classViewBtnActive
+                : classViewBtn,
+            onPressed: () => currentViewMode == ViewMode.classMode
+                ? null
+                : _switchViewMode(),
+          ),
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Tooltip(
+          message: '关系视图',
+          displayHorizontally: true,
+          useMousePosition: false,
+          style: const TooltipThemeData(preferBelow: true),
+          child: IconButton(
+            icon: currentViewMode == ViewMode.relationMode
+                ? relationViewBtnActive
+                : relationViewBtn,
+            onPressed: () => currentViewMode == ViewMode.relationMode
+                ? null
+                : _switchViewMode(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _workBenchController() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Tooltip(
+          message: '浏览区',
+          displayHorizontally: true,
+          useMousePosition: false,
+          style: const TooltipThemeData(preferBelow: true),
+          child: IconButton(
+            icon: isBrowserPaneVisible ? browserPaneBtnActive : browserPaneBtn,
+            onPressed: () => _swichBrowserPane(),
+          ),
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Tooltip(
+          message: '编辑区',
+          displayHorizontally: true,
+          useMousePosition: false,
+          style: const TooltipThemeData(preferBelow: true),
+          child: IconButton(
+            icon: isEditorPaneVisible ? editorPaneBtnActive : editorPaneBtn,
+            onPressed: () => _switchEditorPane(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _functionBtnGroupBuilder() {
+    return Row(
+      children: [
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                flex: 1,
+                child: _browserModeBtnBuilder(),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                flex: 1,
+                child: _workBenchController(),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget _classManagerBuilder() {
     return Padding(
       padding: const EdgeInsets.all(6),
@@ -74,23 +219,29 @@ class _ManagerPageState extends State<ManagerPage> {
           padding: const EdgeInsets.fromLTRB(0, 6, 0, 12),
           child: Row(
             children: [
-              Expanded(
-                flex: 1,
-                child: _classManagerBuilder(),
+              Visibility(
+                visible: isBrowserPaneVisible,
+                child: Expanded(
+                  flex: 1,
+                  child: _classManagerBuilder(),
+                ),
               ),
               Expanded(
-                flex: 2,
+                flex: centerFlexWeight,
                 child: Column(
                   children: [
-                    const FunctionButtonGroup(),
+                    _functionBtnGroupBuilder(),
                     _relationChartBuilder(),
                   ],
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: _editorBuilder(),
-              ),
+              Visibility(
+                visible: isEditorPaneVisible,
+                child: Expanded(
+                  flex: 1,
+                  child: _editorBuilder(),
+                ),
+              )
             ],
           ),
         ),
@@ -98,3 +249,40 @@ class _ManagerPageState extends State<ManagerPage> {
     );
   }
 }
+
+const classViewBtn = Icon(
+  FluentIcons.branch_commit,
+  size: 20,
+);
+final classViewBtnActive = Icon(
+  FluentIcons.branch_commit,
+  color: Colors.blue,
+  size: 20,
+);
+const relationViewBtn = Icon(
+  FluentIcons.branch_fork2,
+  size: 20,
+);
+final relationViewBtnActive = Icon(
+  FluentIcons.branch_fork2,
+  color: Colors.blue,
+  size: 20,
+);
+const browserPaneBtn = Icon(
+  FluentIcons.open_pane,
+  size: 20,
+);
+final browserPaneBtnActive = Icon(
+  FluentIcons.open_pane,
+  color: Colors.blue,
+  size: 20,
+);
+const editorPaneBtn = Icon(
+  FluentIcons.open_pane_mirrored,
+  size: 20,
+);
+final editorPaneBtnActive = Icon(
+  FluentIcons.open_pane_mirrored,
+  color: Colors.blue,
+  size: 20,
+);
