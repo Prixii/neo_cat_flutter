70fd3159ce3cf5c94af23daa0489c480053eb95c
wang.jiaqi <279298470@qq.com>
2023-10-30 19:13:58 +0800

    [feat]: 人工标注视觉效果优化

--- lib/components/initializr/rich_text_field/background_text.dart
+++ lib/components/initializr/rich_text_field/background_text.dart
@@ -47,19 +47,24 @@ class BackgroundText extends SpecialText {
       fullText = fullText.substring(index + 1, fullText.length - 1);
     }
     TextStyle textStyle = this.textStyle ?? defaultText;
+    var background = getColor(label);
     var backgroundTextSpan = SpecialTextSpan(
       children: [
         TextSpan(
           text: fullText,
-          style: textStyle.copyWith(
+          style: calculateTextColor(background).copyWith(
             background: Paint()..color = getColor(label),
+            fontWeight: FontWeight.normal,
+            fontSize: 16,
           ),
           recognizer: TapGestureRecognizer()..onTap = () => fullText = "",
         )
       ],
       text: label,
       style: textStyle.copyWith(
-          fontSize: 8, background: Paint()..color = Colors.transparent),
+        fontSize: 8,
+        background: Paint()..color = Colors.grey[20],
+      ),
       actualText: label,
       start: start,
       deleteAll: true,
